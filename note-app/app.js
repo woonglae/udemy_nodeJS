fa736836e97085eceb2c172927e65366fa98da7c
Woongrae Cho <woonglae@gmail.com>
2019-07-09 23:52:02 -0400

    Add two commands in yargs which are add, remove

--- note-app/app.js
+++ note-app/app.js
@@ -5,6 +5,26 @@ const getNotes = require('./notes.js')
 //custmize yargs version
 yargs.version('1.1.0')
 
+//add, remove, read, list notes
+
+//Add command
+yargs.command({
+    command: 'add',
+    descirbe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note!')
+    }
+})
+
+//Remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    hander: function() {
+        console.log('Removing the note')
+    }
+})
+
 console.log(yargs.argv)
 
 
