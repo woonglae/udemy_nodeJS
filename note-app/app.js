1ea2bc6495c00e480f91016e118108ab36a97190
Woongrae Cho <woonglae@gmail.com>
2019-07-09 23:43:49 -0400

    use yargs getting args

--- note-app/app.js
+++ note-app/app.js
@@ -1,15 +1,6 @@
-const getNotes = require('./notes.js')
 const chalk = require('chalk')
-
-const command = process.argv[2]
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
 
 console.log(process.argv)
-
-if (command === 'add') { 
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
-} else {
-    console.log('please put add or remove')
-}
-
+console.log(yargs.argv)
