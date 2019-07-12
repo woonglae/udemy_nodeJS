7d5839806b7793520f1083d5d56328f1d7628719
Woongrae Cho <woonglae@gmail.com>
2019-07-11 21:48:51 -0400

    Load json file

--- playground/1-jason.js
+++ playground/1-jason.js
@@ -1,11 +1,13 @@
 const fs = require('fs')
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday'
-}
-const bookJSON = JSON.stringify(book)
-fs.writeFileSync('1-json.json', bookJSON)
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday'
+// }
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-json.json', bookJSON)
 
-
+const dataBuffer = fs.readFileSync('1-json.json')
+console.log(dataBuffer)
+console.log(dataBuffer.toString)
 
  
