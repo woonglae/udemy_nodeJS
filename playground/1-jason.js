ac7460f932081ac7cdf55775a3c1f6c43ccc1af6
Woongrae Cho <woonglae@gmail.com>
2019-07-11 21:47:34 -0400

    make jason file using fs

--- playground/1-jason.js
+++ playground/1-jason.js
@@ -1,8 +1,11 @@
+const fs = require('fs')
 const book = {
     title: 'Ego is the Enemy',
     author: 'Ryan Holiday'
 }
-
 const bookJSON = JSON.stringify(book)
+fs.writeFileSync('1-json.json', bookJSON)
 
-console.log(bookJSON)
+
+
+ 
