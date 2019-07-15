2e2b52c8e6da0890c939373c3e40389b8cd0a363
Woongrae Cho <woonglae@gmail.com>
2019-07-14 21:44:18 -0400

    Test until this moment

--- note-app/app.js
+++ note-app/app.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 
 //custmize yargs version
 yargs.version('1.1.0')
@@ -25,7 +25,7 @@ yargs.command({
         }
     },
     handler: function(argv) {
-        console.log('Title: ' + argv.title + ' Body: ' + argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
 
