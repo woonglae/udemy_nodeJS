e433798d77f611dbe0975a47b9d0818b982b1909
Woongrae Cho <woonglae@gmail.com>
2019-07-14 21:34:20 -0400

    change addNote and loadNotes function

--- note-app/notes.js
+++ note-app/notes.js
@@ -1,9 +1,17 @@
+const fs = require('fs')
+
 const getNotes = function() {
     return "Your notes..."
 }
 
 const addNote = function (title, body) {
-    
+    const notes = loadNotes()
+}
+
+const loadNotes = function() {
+    const dataBuffer = fs.readFileSync('notes.json')
+    const dataJSON = dataBuffer.toString()
+    return JSON.parse(dataJSON)
 }
 
 module.exports = {
