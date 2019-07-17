6dc84b7bc4937380f5a818c7d6c6f844cfd59aac
Woongrae Cho <woonglae@gmail.com>
2019-07-17 19:08:16 -0400

    In AddNotes function, change condition duplicateNote

--- note-app/notes.js
+++ note-app/notes.js
@@ -8,8 +8,9 @@ const getNotes = () => {
 const addNote = (title, body) => {
     const notes = loadNotes()
     const duplicateNotes = notes.filter((note) => note.title === title)
-    console.log(duplicateNotes)
-    if (duplicateNotes.length === 0) {
+    const duplicateNote = notes.find((note) => note.title === title)
+    console.log(duplicateNote)
+    if (!duplicateNote) {
         notes.push({
             title: title,
             body: body
