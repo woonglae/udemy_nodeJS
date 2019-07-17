831de955fc8f5a13954d7e475da0adbb028c7712
Woongrae Cho <woonglae@gmail.com>
2019-07-16 21:24:56 -0400

    Arrow function doesn't bind the value in forEach method

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -23,7 +23,7 @@ const event = {
     guestList: ['Ray', 'Alissa'],
     printGuestList() {
         console.log('Guest list for ' + this.name)
-        this.guestList.forEach(function(guest) {
+        this.guestList.forEach((guest) => {
             console.log(guest + ' is attending ' + this.name)
         })
     }
