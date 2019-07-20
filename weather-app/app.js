edad4c0e67d18cdbf7dd4b1dd51d1dcce4e4cff5
Woongrae Cho <woonglae@gmail.com>
2019-07-20 17:41:58 -0400

    Add summary

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,11 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/49a007735cb935977fb6644b2dbb0523/37.8267,-122.4233?units=si'
+const url = 'https://api.darksky.net/forecast/49a007735cb935977fb6644b2dbb0523/37.8267,-122.4233?units=si&'
 
 request({ url: url, json: true }, (error, response) => {
     temp = response.body.currently.temperature
     rainChance = response.body.currently.precipProbability
     console.log("It is currently " + temp + " degrees out. There is a " + rainChance + "% chance of rain.")
+    console.log(response.body.daily.data[0].summary)
 })
 
