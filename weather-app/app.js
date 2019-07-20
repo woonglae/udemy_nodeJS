866d86ec76a58e990c91da07e304f3436bb2f002
Woongrae Cho <woonglae@gmail.com>
2019-07-20 16:40:53 -0400

    request as JSON

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,6 @@ const request = require('request')
 
 const url = 'https://api.darksky.net/forecast/49a007735cb935977fb6644b2dbb0523/37.8267,-122.4233'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.currently)    
 })
