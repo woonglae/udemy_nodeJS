2a1b9ec2cbb17a3d51a6ba13ed07e0b655280be3
Woongrae Cho <woonglae@gmail.com>
2019-07-20 17:39:07 -0400

    Change units to celcious

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,10 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/49a007735cb935977fb6644b2dbb0523/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/49a007735cb935977fb6644b2dbb0523/37.8267,-122.4233?units=si'
 
 request({ url: url, json: true }, (error, response) => {
-    console.log(response.body.timezone)    
+    temp = response.body.currently.temperature
+    rainChance = response.body.currently.precipProbability
+    console.log("It is currently " + temp + " degrees out. There is a " + rainChance + "% chance of rain.")
 })
+
