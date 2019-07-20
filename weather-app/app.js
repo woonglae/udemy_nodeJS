ef3ea583bcd3775d65e7e41bf32a4000ada2f6b6
Woongrae Cho <woonglae@gmail.com>
2019-07-20 16:58:38 -0400

    see the timezone

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,5 +3,5 @@ const request = require('request')
 const url = 'https://api.darksky.net/forecast/49a007735cb935977fb6644b2dbb0523/37.8267,-122.4233'
 
 request({ url: url, json: true }, (error, response) => {
-    console.log(response.body.currently)    
+    console.log(response.body.timezone)    
 })
