e3a6a7d63581717edd9874b69e0c4b46c7add046
Woongrae Cho <woonglae@gmail.com>
2019-07-20 16:26:15 -0400

    import JSON data and we will look it up

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,6 +3,6 @@ const request = require('request')
 const url = 'https://api.darksky.net/forecast/49a007735cb935977fb6644b2dbb0523/37.8267,-122.4233'
 
 request({ url: url }, (error, response) => {
-    console.log(response)
-    
+    const data = JSON.parse(response.body)
+    console.log(data)
 })
