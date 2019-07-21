f2582f8702f670a207194acf6435e33532bd0b8f
Woongrae Cho <woonglae@gmail.com>
2019-07-21 17:31:30 -0400

    Add error handling with if statement

--- weather-app/app.js
+++ weather-app/app.js
@@ -7,20 +7,24 @@ const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/atlanta.js
 // in korean
 // const url = 'https://api.darksky.net/forecast/49a007735cb935977fb6644b2dbb0523/37.8267,-122.4233?units=si&lang=ko'
 
-// request({ url: url, json: true }, (error, response) => {
-//     temp = response.body.currently.temperature
-//     rainChance = response.body.currently.precipProbability
-//     console.log("It is currently " + temp + " degrees out. There is a " + rainChance + "% chance of rain.")
-//     console.log(response.body.daily.data[0].summary)
+request({ url: url, json: true }, (error, response) => {
+    if (error) {
+        console.log('Unable to connect to weather service!')
+    } else {
+        temp = response.body.currently.temperature
+        rainChance = response.body.currently.precipProbability
+        console.log("It is currently " + temp + " degrees out. There is a " + rainChance + "% chance of rain.")
+        console.log(response.body.daily.data[0].summary)
 
-//     //in korean
-//     // console.log("현재 " + temp + " 도이며 비가 올 확률은 " + rainChance + "% 입니다")
-//     // console.log(response.body.daily.data[0].summary)
-// })
-
-request({ url: geocodeURL, json: true}, (error, response) => {
-    longitude = response.body.features[0].center[0]
-    latitude = response.body.features[0].center[1]
-    console.log("longitude : " + longitude + ", latitude : " + latitude)
+        //in korean
+        // console.log("현재 " + temp + " 도이며 비가 올 확률은 " + rainChance + "% 입니다")
+        // console.log(response.body.daily.data[0].summary)
+    }
 })
+
+// request({ url: geocodeURL, json: true}, (error, response) => {
+//     longitude = response.body.features[0].center[0]
+//     latitude = response.body.features[0].center[1]
+//     console.log("longitude : " + longitude + ", latitude : " + latitude)
+// })
 
