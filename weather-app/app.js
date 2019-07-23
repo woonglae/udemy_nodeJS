d607334bc1c92b7030aea507019804d7f23a525f
Woongrae Cho <woonglae@gmail.com>
2019-07-23 12:17:32 -0400

    use callback func in app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,6 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/49a007735cb935977fb6644b2dbb0523/33.7491,-84.3902?units=si'
-
-
-// in korean
-// const url = 'https://api.darksky.net/forecast/49a007735cb935977fb6644b2dbb0523/37.8267,-122.4233?units=si&lang=ko'
+// const url = 'https://api.darksky.net/forecast/49a007735cb935977fb6644b2dbb0523/33.7491,-84.3902?units=si'
 
 // request({ url: url, json: true }, (error, response) => {
 //     if (error) {
@@ -16,25 +12,40 @@ const url = 'https://api.darksky.net/forecast/49a007735cb935977fb6644b2dbb0523/3
 //         rainChance = response.body.currently.precipProbability
 //         console.log("It is currently " + temp + " degrees out. There is a " + rainChance + "% chance of rain.")
 //         console.log(response.body.daily.data[0].summary)
-
-//         //in korean
-//         // console.log("현재 " + temp + " 도이며 비가 올 확률은 " + rainChance + "% 입니다")
-//         // console.log(response.body.daily.data[0].summary)
 //     }
 // })
 
-const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/atlanta.json?proximity=-74.70850,40.78375&access_token=pk.eyJ1Ijoid29vbmdsYWUiLCJhIjoiY2p5YzJjanhtMGY4bTNtczVtM3M1NmdrciJ9.Eg3ivZXMcUAzyJf3yTgWmw&limit=1"
+// const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/atlanta.json?proximity=-74.70850,40.78375&access_token=pk.eyJ1Ijoid29vbmdsYWUiLCJhIjoiY2p5YzJjanhtMGY4bTNtczVtM3M1NmdrciJ9.Eg3ivZXMcUAzyJf3yTgWmw&limit=1"
 
-request({ url: geocodeURL, json: true}, (error, response) => {
+// request({ url: geocodeURL, json: true}, (error, response) => {
+//     if (error) {
+//         console.log("Unable to connect to geographic service!")
+//     } else if (response.body.features.length === 0) {
+//         console.log("Cannot find location information")
+//     } else {
+//         longitude = response.body.features[0].center[0]
+//         latitude = response.body.features[0].center[1]
+//         console.log("longitude : " + longitude + ", latitude : " + latitude)    
+//     }
+
+// })
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?proximity=-74.70850,40.78375&access_token=pk.eyJ1Ijoid29vbmdsYWUiLCJhIjoiY2p5YzJjanhtMGY4bTNtczVtM3M1NmdrciJ9.Eg3ivZXMcUAzyJf3yTgWmw&limit=1'
+
+    request({url: url, json: true}, (error, response) => {
     if (error) {
-        console.log("Unable to connect to geographic service!")
+        callback('Unable to connect to location service!')
     } else if (response.body.features.length === 0) {
-        console.log("Cannot find location information")
+        callback("Cannot find location information")
     } else {
         longitude = response.body.features[0].center[0]
         latitude = response.body.features[0].center[1]
-        console.log("longitude : " + longitude + ", latitude : " + latitude)    
+        callback("longitude : " + longitude + ", latitude : " + latitude) 
     }
+    })
+}
 
+geocode('Philadelphia', (error, data) => {
+    
 })
-
