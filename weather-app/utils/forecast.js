6a84d2ee0bc732b3a567686dd39d1b1b23db61d9
Woongrae Cho <woonglae@gmail.com>
2019-07-24 23:58:27 -0400

    complete the challenge for forecast

---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const forecast = (longitude, latitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/49a007735cb935977fb6644b2dbb0523/' + encodeURIComponent(latitude) + ','+ encodeURIComponent(longitude) + '?units=si'
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (response.body.code === 400) {
+            callback("Unable to find location", undefined)
+        } else {
+            callback(undefined, {
+                temp : response.body.currently.temperature,
+                rainChance : response.body.currently.precipProbability,
+                summary : response.body.daily.data[0].summary,
+                message: "It is currently " + temp + " degrees out. There is a " + rainChance + "% chance of rain."
+            })
+    }
+})
+
+}
