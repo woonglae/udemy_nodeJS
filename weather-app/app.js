5210aa1d2d6806283c55335ae7801aed2bb6e114
Woongrae Cho <woonglae@gmail.com>
2019-07-25 23:39:23 -0400

    Change variable names

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,17 +1,17 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Atlanta', (error, data) => {
+geocode('Atlanta', (error, geocodeData) => {
     if (error) {
         return console.log(error)
     } else {
-        forecast(data.longitude, 
-            data.latitude, (error, data) => {
+        forecast(geocodeData.longitude, 
+            geocodeData.latitude, (error, forecastData) => {
                 if (error) {
                     return console.log(error)
                 } else {
                     console.log('Error', error)
-                    console.log('Data', data)
+                    console.log('Data', forecastData)
                 }
         })    
     }    
