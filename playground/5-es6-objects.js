a333cc38135638360b4cffe833f904dc1679ba86
Woongrae Cho <woonglae@gmail.com>
2019-07-26 03:33:00 -0400

    destructuring object

--- playground/5-es6-objects.js
+++ playground/5-es6-objects.js
@@ -8,4 +8,25 @@ const user = {
     location: "Atlanta"
 }
 
-console.log(user)
+// Object destructuring
+
+const product = {
+    label: 'Red notebook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined
+}
+
+// // assigning inside {} is default value (for price)
+// const {label, price = 0, stock: leftStock, rating} = product
+
+// console.log(label)
+// console.log(price)
+// console.log(leftStock)
+// console.log(rating)
+
+const transaction = (type, {label, stock}) => {
+    console.log(type, label, stock)
+}
+
+transaction('order', product)
