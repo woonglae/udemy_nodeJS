43755bafef040f5215a609cd733ccad122c5aa0b
Woongrae Cho <woonglae@gmail.com>
2019-07-25 23:48:29 -0400

    Add error handler for the code block

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,18 +3,23 @@ const forecast = require('./utils/forecast')
 
 const location = process.argv[2]
 
-geocode(location, (error, geocodeData) => {
-    if (error) {
-        return console.log(error)
-    } else {
-        forecast(geocodeData.longitude, 
-            geocodeData.latitude, (error, forecastData) => {
-                if (error) {
-                    return console.log(error)
-                } else {
-                    console.log(geocodeData.location)
-                    console.log( forecastData)
-                }
-        })    
-    }    
-})
+if(!location) {
+    console.log("Please put the location you want to find")
+} else {
+    geocode(location, (error, geocodeData) => {
+        if (error) {
+            return console.log(error)
+        } else {
+            forecast(geocodeData.longitude, 
+                geocodeData.latitude, (error, forecastData) => {
+                    if (error) {
+                        return console.log(error)
+                    } else {
+                        console.log(geocodeData.location)
+                        console.log( forecastData)
+                    }
+            })    
+        }    
+    })
+}
+
