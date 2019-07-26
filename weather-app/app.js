296af21d743a8f33b17d5827367b64916bcf6a99
Woongrae Cho <woonglae@gmail.com>
2019-07-25 23:38:16 -0400

    Add error handling in each function

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,10 +2,17 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 geocode('Atlanta', (error, data) => {
-    console.log('Error: ', error)
-    console.log('Data: ', data)
-    forecast(data.longitude, data.latitude, (error, data) => {
-        console.log('Error', error)
-        console.log('Data', data)
-      })    
+    if (error) {
+        return console.log(error)
+    } else {
+        forecast(data.longitude, 
+            data.latitude, (error, data) => {
+                if (error) {
+                    return console.log(error)
+                } else {
+                    console.log('Error', error)
+                    console.log('Data', data)
+                }
+        })    
+    }    
 })
