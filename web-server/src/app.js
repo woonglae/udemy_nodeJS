eae782a2757a6f95465cd72f2c4cbc5db029914c
Woongrae Cho <woonglae@gmail.com>
2019-07-27 18:20:45 -0400

    using express, request server

---
+++ web-server/src/app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+
+const app = express()
+
+// app.com
+app.get('', (req, res) => {
+    res.send('<h1>Head</h1>')
+})
+
+// app.com/help
+app.get('/help', (req, res) => {
+    res.send([{
+        name: 'Andrew', 
+        age: 27
+    }, {
+        name: 'Ray',
+        age: 29
+    }])
+})
+
+// app.com/about
+app.get('/about', (req, res) => {
+    res.send('<h1>About</h1>')
+})
+
+// app.com/weather
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'Partly Cloud',
+        location: 'Atlanta'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
