c5140179abb846affe82bd7639114cafe9a62e7f
Woongrae Cho <woonglae@gmail.com>
2019-07-27 23:29:50 -0400

    render the hbs file in app.js

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -8,6 +8,11 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
+app.get('', (req, res) => {
+    // need to match filename in the views folder and the string in res.render() 
+    res.render('index')
+})
+
 // app.com/weather
 app.get('/weather', (req, res) => {
     res.send({
