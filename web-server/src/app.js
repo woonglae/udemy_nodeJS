d75936e4a372d754a7cf009f2ec72e695bad06a7
Woongrae Cho <woonglae@gmail.com>
2019-07-29 13:21:07 -0400

    Add header

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,18 @@
 const path = require('path')
 const express = require('express')
-const hbs = request('hbs')
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
